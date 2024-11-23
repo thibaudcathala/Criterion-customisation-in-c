c6c4e37d3fcf406f1152e3408c7902594ac5f486
thibaudcathala <thibaud.cathala@epitech.eu>
2024-11-23 19:53:03 +0100

    add: my_test.c

---
+++ my_test.c
@@ -0,0 +1,259 @@
+#include <criterion/criterion.h>
+
+TestSuite(MyTestSuite);
+Test(MyTest1, MyTest1Description)
+{
+    cr_assert(0 == 1);
+}
+
+Test(MyTest2, MyTest2Description)
+{
+    cr_assert(0 == 1);
+}
+
+Test(MyTest3, MyTest3Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest4, MyTest4Description)
+{
+    cr_assert(0 == 1);
+}
+
+Test(MyTest5, MyTest5Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest6, MyTest6Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest7, MyTest7Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest8, MyTest8Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest9, MyTest9Description)
+{
+    cr_assert(0 == 1);
+}
+
+Test(MyTest10, MyTest10Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest11, MyTest11Description)
+{
+    cr_assert(0 == 1);
+}
+
+Test(MyTest12, MyTest12Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest13, MyTest13Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest14, MyTest14Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest15, MyTest15Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest16, MyTest16Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest17, MyTest17Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest18, MyTest18Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest19, MyTest19Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest20, MyTest20Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest21, MyTest21Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest22, MyTest22Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest23, MyTest23Description)
+{
+    cr_assert(0 == 1);
+}
+
+Test(MyTest24, MyTest24Description)
+{
+    cr_assert(0 == 1);
+}
+
+Test(MyTest25, MyTest25Description)
+{
+    cr_assert(0 == 1);
+}
+
+Test(MyTest26, MyTest26Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest27, MyTest27Description)
+{
+    cr_assert(0 == 1);
+}
+
+Test(MyTest28, MyTest28Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest29, MyTest29Description)
+{
+    cr_assert(0 == 1);
+}
+
+Test(MyTest30, MyTest30Description)
+{
+    cr_assert(0 == 1);
+}
+
+Test(MyTest31, MyTest31Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest32, MyTest32Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest33, MyTest33Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest34, MyTest34Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest35, MyTest35Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest36, MyTest36Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest37, MyTest37Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest38, MyTest38Description)
+{
+    cr_assert(0 == 1);
+}
+
+Test(MyTest39, MyTest39Description)
+{
+    cr_assert(0 == 1);
+}
+
+Test(MyTest40, MyTest40Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest41, MyTest41Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest42, MyTest42Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest43, MyTest43Description)
+{
+    cr_assert(0 == 1);
+}
+
+Test(MyTest44, MyTest44Description)
+{
+    cr_assert(0 == 1);
+}
+
+Test(MyTest45, MyTest45Description)
+{
+    cr_assert(0 == 1);
+}
+
+Test(MyTest46, MyTest46Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest47, MyTest47Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest48, MyTest48Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTest49, MyTest49Description)
+{
+    cr_assert(0 == 1);
+}
+
+Test(MyTest50, MyTest50Description)
+{
+    cr_assert(0 == 0);
+}
+
+Test(MyTestCrash, MyTestCrash)
+{
+    int *test = NULL;
+    *test = 42;
+    cr_assert(0 == 0);
+}
